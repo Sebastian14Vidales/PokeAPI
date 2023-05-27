1006ee4fc4abdae1f09c2e4156530a67b0d05e10
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-05-27 13:03:27 -0500

    Adelantando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { getAleatorio, getPokemon } from "./components/PokemonAleatorio";
 
 function App() {
-
-  
   const [poke, setPoke] = useState(0);
 
   function handleClick(e) {
@@ -13,41 +11,26 @@ function App() {
     getPokemon(id);
   }
 
-  // const pokemon = {
-  //   img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png`,
-  //   imgJuego: poke.sprites.front_default,
-  //   imgCvg: poke.sprites.other.dream_world.front_default,
-  //   nombre: poke.name,
-  //   experiencia: poke.base_experience,
-  //   hp: poke.stats[0].base_stat,
-  //   ataque: poke.stats[1].base_stat,
-  //   defensa: poke.stats[2].base_stat,
-  //   especial: poke.stats[3].base_stat,
-  // };
-  
   return (
     <>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src=""
-            alt=""
-          />
-        </a>
+        <img
+          className="rounded mt-6 mx-auto imagen-pokemon bg-cover bg-center"
+          src=""
+          alt=""
+        />
         <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <h5 className="mb-2 titulo uppercase text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+
+          <p className="mb-3 experiencia font-normal text-gray-700 dark:text-gray-400">
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
           <a
             href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="flex uppercase justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={handleClick}
           >
             Generar Pokemon
--- src/components/PokemonAleatorio.jsx
+++ src/components/PokemonAleatorio.jsx
@@ -6,8 +6,31 @@ async function getPokemon(id) {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const respuesta = await fetch(url);
     const datos = await respuesta.json();
-    console.log(datos);
+    mostrarDatos(datos);
+}
 
+function mostrarDatos(datos) {
+    const {sprites, name, base_experience, stats, id} = datos;
+    const pokemon = {
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+    imgJuego: sprites.front_default,
+    imgCvg: sprites.other.dream_world.front_default,
+    nombre: name,
+    experiencia: base_experience,
+    hp: stats[0].base_stat,
+    ataque: stats[1].base_stat,
+    defensa: stats[2].base_stat,
+    especial: stats[3].base_stat,
+  };
+
+  const imagen = document.querySelector('.imagen-pokemon');
+  const titulo = document.querySelector('.titulo');
+//   const experiencia = document.querySelector('.experiencia');
+  imagen.src = `${pokemon.img}`;
+  imagen.alt = `Imagen ${name}`;
+  titulo.textContent = `${name}`
+//   experiencia.textContent = `${especial}`;
+    console.log(imagen);
 }
 
 export {
