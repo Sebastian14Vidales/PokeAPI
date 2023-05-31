d7c12315161200242f00cc70b1f5529da817172b
Sebastian Vidales <vidales14sebastian@gmail.com>
2023-05-30 22:52:48 -0500

    Ajustando card

--- src/App.jsx
+++ src/App.jsx
@@ -13,27 +13,27 @@ function App() {
 
   return (
     <>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img
-          className="rounded mt-6 mx-auto imagen-pokemon bg-cover bg-center"
-          src=""
-          alt=""
-        />
+      <div className="max-w-sm w-96 bg-white border border-gray-200 rounded-lg shadow-sm shadow-slate-400 dark:bg-gray-800 dark:border-gray-700">
+        <div className="imagen-pokemon">
+        </div>
         <div className="p-5">
-          <h5 className="mb-2 titulo uppercase text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p className="mb-2 titulo text-center uppercase text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 experiencia font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
           </p>
+          
+          <div className="pokeInfo">
+            <div className="mb-6 experiencia font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </div>
+          </div>
           <a
             href="#"
-            className="flex uppercase justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="flex uppercase justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
             onClick={handleClick}
           >
             Generar Pokemon
+            <img className="ms-2 w-5" src="../public/poke.svg" alt="pokebola" />
           </a>
         </div>
       </div>
--- src/components/PokemonAleatorio.jsx
+++ src/components/PokemonAleatorio.jsx
@@ -1,17 +1,20 @@
 function getAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 async function getPokemon(id) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const respuesta = await fetch(url);
-    const datos = await respuesta.json();
-    mostrarDatos(datos);
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
+  mostrarDatos(datos);
 }
 
 function mostrarDatos(datos) {
-    const {sprites, name, base_experience, stats, id} = datos;
-    const pokemon = {
+  const divImagen = document.querySelector(".imagen-pokemon");
+  limpiarHTML(divImagen);
+
+  const { sprites, name, base_experience, stats, id } = datos;
+  const pokemon = {
     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
     imgJuego: sprites.front_default,
     imgCvg: sprites.other.dream_world.front_default,
@@ -23,18 +26,39 @@ function mostrarDatos(datos) {
     especial: stats[3].base_stat,
   };
 
-  const imagen = document.querySelector('.imagen-pokemon');
-  const titulo = document.querySelector('.titulo');
-//   const experiencia = document.querySelector('.experiencia');
-  imagen.src = `${pokemon.img}`;
-  imagen.alt = `Imagen ${name}`;
-  titulo.textContent = `${name}`
-//   experiencia.textContent = `${especial}`;
-    console.log(imagen);
+  const imagen = document.createElement("img");
+  const titulo = document.querySelector(".titulo");
+  
+  const experiencia = document.querySelector(".experiencia"); 
+
+  divImagen.classList.add('relative', 'bg-gray-300', 'h-44', 'mx-0')
+  imagen.src = `${pokemon.imgCvg}`;
+  imagen.alt = `Imagen ${pokemon.nombre}`;
+  imagen.classList.add('rounded','absolute', 'top-0', 'left-8', 'mt-6', 'px-10', 'my-0', 'mx-auto', 'bg-cover', 'bg-center', 'w-80', 'h-60');
+  titulo.classList.add('mt-24');
+  titulo.innerHTML = `
+    ${pokemon.nombre}
+    <span class="text-gray-400 text-base">${pokemon.hp}hp</span>
+    <h2 class="text-lg text-blue-700">${pokemon.experiencia} <span class="text-sm">exp</span> </h2>
+    `;
+  divImagen.appendChild(imagen);
+  console.log(divImagen);
+
+  experiencia.classList.add('flex', 'justify-between', 'py-4', 'border-t', 'border-gray-200')
+  experiencia.innerHTML = `
+    <h2 class="uppercase font-bold text-center text-blue-700">Ataque <br><span class="text-gray-400">${pokemon.ataque}K</span></h2> 
+    <h2 class="uppercase font-bold text-center text-blue-700">Defensa <br><span class="text-gray-400">${pokemon.defensa}K</span></h2> 
+    <h2 class="uppercase font-bold text-center text-blue-700">Especial <br><span class="text-gray-400">${pokemon.especial}K</span></h2> 
+    
+  `;
+  
+  console.log(imagen);
 }
 
-export {
-    getAleatorio,
-    getPokemon
+function limpiarHTML(divImagen) {
+  while (divImagen.firstChild) {
+    divImagen.removeChild(divImagen.firstChild);
+  }
 }
 
+export { getAleatorio, getPokemon };
